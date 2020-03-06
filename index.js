55fbf2b0bc1d7bdc63bd5e801990ba263cce6006
leslie zhu <zhujunshuai@huohua.cn>
2020-03-06 21:30:19 +0800

    add koa framework

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const http = require("http");
-const url = require("url");
+const koa = require("koa");
 const chalk = require("chalk");
+const app = new koa();
 
-const listener = function(req, res) {
-  const { url } = req;
-  res.end(url);
-};
+app.use(async (ctx, next) => {
+  console.log(ctx);
+});
 
-const server = http.createServer(listener);
-
-server.listen(5000, "localhost", () => {
-  console.log(chalk.green("服务器工作在：http://localhost:5000"));
+app.listen(5000, () => {
+  console.log(chalk.green("服务器工作在：localhost:5000"));
 });
